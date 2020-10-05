ada4adb142f9457fc99cc055cbf32c6aef634626
iddimsangi <adammsangi@gmail.com>
2020-10-05 19:33:59 +0300

    add all webpack and babel dependencies

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+import str from './model/Search';
+import{add,multiply,num} from './view/SearchView';
+console.log(`${str} and the sum is ${add(num,2)} and the multiplication result is ${multiply(2,3)}`);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry:['@babel/polyfill','./src/js/index.js'],
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'js/bundle.js'
+    },
+    mode:'development',
+    devServer:{
+        contentBase:'./dist'
+    },
+    plugins:[
+   new HtmlWebPackPlugin({
+       filename:'index.html',
+       template:'./src/index.html'
+   })
+    ],
+    module: {
+        rules: [
+                {
+                    test: /\.js$/,
+                    use: ['babel-loader'],
+                    exclude: '/node_modules/'
+                }
+            // {
+            //     test: /\.(scss|sass)$/,
+            //     use: ExtractTextPlugin.extract({
+            //         use: ['css-loader', 'sass-loader']  
+            //     })
+            // }
+        ]
+    }
+}
