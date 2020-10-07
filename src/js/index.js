7d8658bcb52986a5b3cb56e4eff0cec85c92a20a
iddimsangi <adammsangi@gmail.com>
2020-10-07 11:33:06 +0300

    add state object and eventListener for our submit form

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,33 @@
 import Search from './model/Search';
-const val = new Search('pizza');//new object with search query property
-console.log(val);
-val.getData();
+// const val = new Search('pizza');//new object with search query property
+
+//create a gloal state for storing our objects
+const state = {
+
+}
+const controlSearch = async() =>{
+    //1.get a query from view
+    const query = 'pizza';//leter will be user input query
+if(query){
+    //2.get new search obj and add to state
+    state.Search = new Search(query);//store this created object in state
+    //3.prepare the ui for result
+
+    //4.search for recipes
+    await state.Search.getData();
+
+    //5.sarender on UI
+    console.log(state.Search.result);
+
+
+}
+}
+
+//App startup 
+document.querySelector('.search').addEventListener('submit',e =>{
+e.preventDefault();//prevent loading
+//function for todo-list
+controlSearch();
+});
+// console.log(val);
+// val.getData();
