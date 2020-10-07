c75c4ddda58bda1bbe5bb94333f2523769e74063
iddimsangi <adammsangi@gmail.com>
2020-10-07 10:55:12 +0300

    create a object with query search property based on expoerted class

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import Search from './model/Search';
-const val = new Search('pizza');
+const val = new Search('pizza');//new object with search query property
 console.log(val);
+val.getData();
--- src/js/model/Search.js
+++ src/js/model/Search.js
@@ -8,6 +8,8 @@ async getData(){
     try {
         const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
         console.log(res);
+        this.result = res.data.recipes;
+        console.log(this.result);
     } catch (error) {
         console.log(error);
     }
